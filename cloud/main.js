1a2abe37cee3ed1917cb99cd68094f28be6bf726
Sam Bauch <sbauch@gmail.com>
2013-08-17 18:28:22 -0400

    friday wrap

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,9 @@
-
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+Parse.Cloud.afterSave(Parse.User, function(request, response) {
+	var user = request.object;
+	if (user.get('water_count') === 8) {
+		console.log('reached 8');
+		// reward badge
+		
+	// TODO: create a post where the type is badge, the text says "user xyz earned the water badge for drinking 8 cups of water today"
+	}
 });
